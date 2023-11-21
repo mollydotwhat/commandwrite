c10d50636bfd5d00b5080f14743aad5a6271eb08
molly <molly.salzberger@gmail.com>
2023-11-20 18:37:25 -0600

    deleted relevant todo notes

--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,5 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// render badge according to user choice
 function renderLicenseBadge(answers) {
   var licensePicked = answers.license;
   var licenseBadgeCode; //backticks, markdown img link
@@ -19,8 +18,7 @@ function renderLicenseBadge(answers) {
   return licenseBadgeCode;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// render link according to user choice
 function renderLicenseLink(answers) {
   var licensePicked = answers.license;
   var licenseLink;
